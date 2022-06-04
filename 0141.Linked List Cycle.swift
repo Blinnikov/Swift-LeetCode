6e0a47e015463613fd609a33715c5ba807f95ec7
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-06-04 19:58:54 +0300

    Create 0141.Linked List Cycle.swift

---
+++ 0141.Linked List Cycle.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    private let nonExistVal = 100001
+    
+    func hasCycle(_ head: ListNode?) -> Bool {
+        if head == nil {
+            return false
+        }
+        
+        var curr: ListNode? = head
+        while curr != nil && curr!.val != nonExistVal {
+            // print("Checking \(curr!.val)")
+            curr!.val = nonExistVal
+            curr = curr!.next
+        }
+        
+        return curr != nil
+    }
+}
