e60cd201a588d5aa2cb4b47ac40e4734db632d50
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-06-05 23:22:45 +0300

    Create 0326.Power of Three.swift

---
+++ 0326.Power of Three.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        n > 0 && 1162261467 % n == 0
+        // (log10(Double(n))/log10(3.0)).truncatingRemainder(dividingBy: 1.0) == 0
+        
+//         if n == 0 {
+//             return false
+//         }
+        
+//         var n = n
+        
+//         while true {
+//             if n == 1 {
+//                 return true 
+//             }
+            
+//             if n%3 != 0 {
+//                 return false
+//             }
+            
+//             n = n / 3
+//         }
+    }
+}
