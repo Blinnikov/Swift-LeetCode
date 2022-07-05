22229769cb831e097a4662cad95a6efdd2c5138a
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-07-05 11:43:03 +0300

    Update 0128.Longest Consecutive Sequence.swift

--- 0128.Longest Consecutive Sequence.swift
+++ 0128.Longest Consecutive Sequence.swift
@@ -4,6 +4,32 @@ class Solution {
             return nums.count
         }
         
+        let set = Set(nums)
+        var maxLen = 1
+        
+        for el in set {
+            if set.contains(el - 1) {
+                continue
+            }
+            
+            var currEl = el
+            var currLen = 1
+            while set.contains(currEl+1) {
+                currEl += 1
+                currLen += 1
+            }
+            
+            maxLen = max(maxLen, currLen)
+        }
+        
+        return maxLen
+    }
+    
+    private func longestConsecutiveSort(_ nums: [Int]) -> Int {
+        if nums.count <= 1 {
+            return nums.count
+        }
+        
         let nums = nums.sorted()
         
         var minPos = -1
