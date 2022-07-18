0a75e9cf17fd56014fed00d9c4ed92819b03ac0f
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-07-19 00:00:35 +0300

    Create 0062.Unique Paths.swift

---
+++ 0062.Unique Paths.swift
@@ -0,0 +1,38 @@
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        if m == 1 || n == 1 {
+            return 1
+        }
+        
+        var dp = Array(repeating: Array(repeating: 1, count: n), count: m)
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            }
+        }
+        
+        return dp[m-1][n-1]
+    }
+    
+    // TLE
+    func uniquePathsRecursive(_ m: Int, _ n: Int) -> Int {
+        if m == 1 && n == 1 {
+            return 1
+        }
+        
+        return find(m-1, n-1)
+    }
+    
+    private func find(_ i: Int, _ j: Int) -> Int {
+        if i == 0 && j == 0 {
+            return 0
+        }
+        
+        if j == 0 || i == 0 {
+            return 1
+            // return find(i-1, j) + 1
+        }
+        
+        return find(i, j-1) + find(i-1, j)
+    }
+}
