142f19a1c8af1eadc55edbc23832a1e0f9239bfa
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-07-31 22:43:16 +0300

    Update 0189.Rotate Array.swift

--- 0189.Rotate Array.swift
+++ 0189.Rotate Array.swift
@@ -1,5 +1,26 @@
 class Solution {
     func rotate(_ nums: inout [Int], _ k: Int) {
+        var k = k % nums.count
+        reverse(&nums, 0, nums.count-k-1)
+        reverse(&nums, nums.count-k, nums.count-1)
+        reverse(&nums, 0, nums.count-1)
+    }
+    
+    private func reverse(_ nums: inout [Int], _ lo: Int, _ hi: Int) {
+        var lo = lo, hi = hi
+        var temp: Int
+        
+        while lo < hi {
+            temp = nums[lo]
+            nums[lo] = nums[hi]
+            nums[hi] = temp
+            
+            lo += 1
+            hi -= 1
+        }
+    }
+    
+    func rotate1(_ nums: inout [Int], _ k: Int) {
         if k == 0 {
             return
         }
