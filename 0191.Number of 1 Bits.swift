6ea86c3e0f969cb5865ab7c9ad6c71ada0d4347d
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-08-07 18:02:24 +0300

    Update 0191.Number of 1 Bits.swift

--- 0191.Number of 1 Bits.swift
+++ 0191.Number of 1 Bits.swift
@@ -1,5 +1,19 @@
 class Solution {
     func hammingWeight(_ n: Int) -> Int {
+        n.nonzeroBitCount
+    }
+    
+    private func hammingWeight_v2(_ n: Int) -> Int {
+        var result = 0
+        var n = n
+        for _ in 0..<32 {
+            result += n & 1
+            n >>= 1
+        }
+        return result
+    }
+    
+    private func hammingWeight_v1(_ n: Int) -> Int {
         var n = n
         var count = 0
         while n > 0 {
