9e2843bf204d8a2dd52268ad4094dc29b29b442c
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-08-13 13:57:13 +0300

    Update 1646.Get Maximum in Generated Array.swift

--- 1646.Get Maximum in Generated Array.swift
+++ 1646.Get Maximum in Generated Array.swift
@@ -8,15 +8,12 @@ class Solution {
         nums[1] = 1
         
         for i in stride(from: 1, through: (n-1)/2, by: 1) {
-            // print("2*i=\(2*i), 2*i+1=\(2*i+1)")
             nums[2*i] = nums[i]
             nums[2*i+1] = nums[i] + nums[i+1]
             
             res = max(res, nums[2*i])
             res = max(res, nums[2*i+1])
         }
-        
-        // print(res)
         
         return res
     }
