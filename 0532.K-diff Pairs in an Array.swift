1f3da40dfac1d722bf2a8da8cf71a556b46bb7a5
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-08-29 19:22:02 +0300

    Update 0532.K-diff Pairs in an Array.swift

--- 0532.K-diff Pairs in an Array.swift
+++ 0532.K-diff Pairs in an Array.swift
@@ -1,5 +1,42 @@
 class Solution {
+    
     func findPairs(_ nums: [Int], _ k: Int) -> Int {
+        var map = [Int:Int]()
+        
+        for num in nums {
+            map[num, default: 0] += 1
+        }
+        
+        var res = 0
+        
+        for key in map.keys {
+            if (k == 0 && map[key, default: 0] > 1) || (k > 0 && map[key+k] != nil) {
+                res += 1
+            }
+        }
+        
+        return res
+    }
+    
+    func findPairsSet(_ nums: [Int], _ k: Int) -> Int {
+        var seen = Set<Int>()
+        var ans = Set<Int>()
+        
+        for num in nums {
+            if seen.contains(num + k), !ans.contains(num + k) {
+                ans.insert(num + k)
+            }
+            if k != 0, seen.contains(num - k), !ans.contains(num) {
+                ans.insert(num)
+            }
+            seen.insert(num)
+            // print(seen, ans)
+        }
+        
+        return ans.count
+    }
+    
+    func findPairsSquare(_ nums: [Int], _ k: Int) -> Int {
         var set = Set<Pair>()
         
 //         set.insert(Pair(5,3))
