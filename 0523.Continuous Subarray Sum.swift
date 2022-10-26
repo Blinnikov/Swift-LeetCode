858ffcb7799bb55829115dc1cdf72afcc18a1d59
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-10-26 10:36:35 +0300

    Create 0523.Continuous Subarray Sum.swift

---
+++ 0523.Continuous Subarray Sum.swift
@@ -0,0 +1,47 @@
+class Solution {
+    func checkSubarraySum(_ nums: [Int], _ k: Int) -> Bool {
+        var modulos = [Int:Int]()
+        modulos[0] = 0
+        
+        var ps = 0
+        for i in 0..<nums.count {
+            ps += nums[i]
+            let mod = ps % k
+            
+            if let exist = modulos[mod] {
+                if exist < i {
+                    return true
+                }
+            } else {
+                modulos[mod] = i + 1
+            }
+        }
+        
+        return false
+    }
+    
+    private func checkSubarraySum_TLE(_ nums: [Int], _ k: Int) -> Bool {
+        var pa = Array(repeating: 0, count: nums.count + 1)
+        pa[0] = 0
+        
+        for i in 1..<pa.count {
+            pa[i] = pa[i-1] + nums[i-1]
+        }
+        
+        // print(pa)
+        
+        for i in 0..<pa.count - 2 {
+            for j in i + 2..<pa.count {
+                let sum = pa[j] - pa[i]
+                
+                // print("Sum(\(i)..\(j)) == \(sum)")
+                
+                if sum % k == 0 {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+}
