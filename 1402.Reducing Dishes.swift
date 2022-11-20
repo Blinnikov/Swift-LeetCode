60fe3f8ad2aeb21f2c042825a37a4b6709c8fba0
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-11-20 20:00:27 +0300

    Create 1402.Reducing Dishes.swift

---
+++ 1402.Reducing Dishes.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func maxSatisfaction(_ satisfaction: [Int]) -> Int {
+        let s = satisfaction.sorted()
+        var total = 0
+        var res = 0
+
+        var i = s.count - 1
+        while i >= 0 && s[i] > -1 * total {
+            total += s[i]
+            res += total
+
+            i -= 1
+        }
+
+        return res
+    }
+}
