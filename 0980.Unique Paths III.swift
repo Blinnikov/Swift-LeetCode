d82083d41463f0f1e1b305f3c2aa7a1f396a3a50
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-11-20 19:51:15 +0300

    Create 0980.Unique Paths III.swift

---
+++ 0980.Unique Paths III.swift
@@ -0,0 +1,44 @@
+class Solution {
+    private var grid = [[Int]]()
+
+    func uniquePathsIII(_ grid: [[Int]]) -> Int {
+        self.grid = grid
+
+        var row = 0, col = 0
+        var emptyCount = 0
+
+        for r in 0..<grid.count {
+            for c in 0..<grid[0].count {
+                if grid[r][c] == 0 {
+                    emptyCount += 1
+                } else if grid[r][c] == 1 {
+                    row = r
+                    col = c
+                }
+            }
+        }
+
+        return dfs(row, col, -1, emptyCount)
+    }
+
+    private func dfs(_ row: Int, _ col: Int, _ current: Int, _ total: Int) -> Int {
+        if row < 0 || row == grid.count || col < 0 || col == grid[0].count || grid[row][col] == -1 {
+            return 0
+        }
+
+        if grid[row][col] == 2 {
+            return current == total ? 1 : 0
+        }
+
+        grid[row][col] = -1
+
+        var res = dfs(row - 1, col, current + 1, total)
+        res += dfs(row, col + 1, current + 1, total)
+        res += dfs(row + 1, col, current + 1, total)
+        res += dfs(row, col - 1, current + 1, total)
+
+        grid[row][col] = 0;
+        
+        return res
+    }
+}
