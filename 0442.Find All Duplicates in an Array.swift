2a0531d7d8aa96c77c6e9ef25d33546fb694aa44
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2022-11-23 14:40:45 +0300

    Update 0442.Find All Duplicates in an Array.swift

--- 0442.Find All Duplicates in an Array.swift
+++ 0442.Find All Duplicates in an Array.swift
@@ -1,5 +1,21 @@
 class Solution {
     func findDuplicates(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        var result = [Int]()
+
+        for num in nums {
+            let idx = num - 1
+            if nums[idx] < 0 {
+                result.append(abs(num))
+            } else {
+                nums[idx] *= -1
+            }
+        }
+
+        return result
+    }
+
+    func findDuplicates_v1(_ nums: [Int]) -> [Int] {
         var set = Set<Int>()
         var result = [Int]()
 
