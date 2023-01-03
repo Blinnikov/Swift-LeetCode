362a93290162e740a6a3c9514ac5d25f52d675a0
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-01-03 15:58:32 +0300

    Create 1470.Shuffle the Array.swift

---
+++ 1470.Shuffle the Array.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+        var result = Array(repeating: 0, count: 2 * n)
+        for i in 0..<n {
+            result[2 * i] = nums[i]
+            result[2 * i + 1] = nums[i + n]
+        }
+
+        return result
+    }
+}
