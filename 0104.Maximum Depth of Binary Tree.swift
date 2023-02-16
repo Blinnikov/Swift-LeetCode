51f8b4b4e43846bc1d28159cbbfee29aa07b6352
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-02-16 11:26:23 +0300

    Create 0104.Maximum Depth of Binary Tree.swift

---
+++ 0104.Maximum Depth of Binary Tree.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        return depth(root)
+    }
+    
+    func depth(_ node: TreeNode?) -> Int {
+        guard node != nil else {
+            return 0
+        }
+        
+        let left = depth(node!.left)
+        let right = depth(node!.right)
+        return max(left,right) + 1
+    }
+}
