7ab06a264a67880f4aa72178892f22c981a503ca
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-02-21 10:25:33 +0300

    Update 0540.Single Element in a Sorted Array.swift

--- 0540.Single Element in a Sorted Array.swift
+++ 0540.Single Element in a Sorted Array.swift
@@ -1,6 +1,19 @@
 class Solution {
+    // O(log n) time and O(1) space
     func singleNonDuplicate(_ nums: [Int]) -> Int {
+        var lo = 0
+        var hi = nums.count - 1
 
+        while lo < hi {
+            let mid = (lo + hi) / 2
+            if (mid & 1 == 1 && nums[mid] == nums[mid - 1]) || (mid & 1 == 0 && nums[mid] == nums[mid + 1]) {
+                lo = mid + 1
+            } else {
+                hi = mid
+            }
+        }
+
+        return nums[lo]
     }
 
     // O(n) time and O(n) space
@@ -17,5 +30,5 @@ class Solution {
         }
 
         return -1
-    }
+    }  
 }
