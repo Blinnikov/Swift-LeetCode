6d6310e629269466eb71bf7e57eded3bff567d49
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-03-06 10:17:57 +0300

    Update 1539.Kth Missing Positive Number.swift

--- 1539.Kth Missing Positive Number.swift
+++ 1539.Kth Missing Positive Number.swift
@@ -1,5 +1,22 @@
 class Solution {
     func findKthPositive(_ arr: [Int], _ k: Int) -> Int {
+        var lo = 0
+        var hi = arr.count
+
+        while lo < hi {
+            let mid = (lo + hi) / 2
+
+            if arr[mid] - 1 - mid < k {
+                lo = mid + 1
+            } else {
+                hi = mid
+            }
+        }
+
+        return lo + k
+     }
+
+    func findKthPositive_On(_ arr: [Int], _ k: Int) -> Int {
         var set = Set(arr)
 
         var idx = 0
