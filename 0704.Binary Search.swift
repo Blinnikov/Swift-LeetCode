f9b4013f9b2d4ea5035bbfc8e890b017c6434778
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-04-01 14:46:11 +0300

    Update 0704.Binary Search.swift

--- 0704.Binary Search.swift
+++ 0704.Binary Search.swift
@@ -23,4 +23,25 @@ class Solution {
             return binarySearch(nums, target, lo: mid+1, hi: hi)
         }
     }
+    
+    func search_iterative(_ nums: [Int], _ target: Int) -> Int {
+        var lo = 0
+        var hi = nums.count - 1
+
+        while lo <= hi {
+            let mid = (lo + hi) / 2
+
+            if nums[mid] == target {
+                return mid
+            }
+
+            if nums[mid] < target {
+                lo = mid + 1
+            } else {
+                hi = mid - 1
+            }
+        }
+
+        return -1
+    }
 }
