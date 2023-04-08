9cb216053b28bcecf32cb5412f07606d68df881e
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-04-08 19:57:57 +0300

    Update 0133.Clone Graph.swift

--- 0133.Clone Graph.swift
+++ 0133.Clone Graph.swift
@@ -1,15 +1,3 @@
-/**
- * Definition for a Node.
- * public class Node {
- *     public var val: Int
- *     public var neighbors: [Node?]
- *     public init(_ val: Int) {
- *         self.val = val
- *         self.neighbors = []
- *     }
- * }
- */
-
 class Solution {
     func cloneGraph(_ node: Node?) -> Node? {
         if node == nil {
