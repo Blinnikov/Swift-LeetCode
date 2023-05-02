ce9def90566183d3d10c408c0173e3ccadaa66d3
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-05-02 16:28:31 +0300

    Create 1822.Sign of the Product of an Array.swift

---
+++ 1822.Sign of the Product of an Array.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func arraySign(_ nums: [Int]) -> Int {
+        var negativeCount = 0
+
+        for num in nums {
+            if num == 0 {
+                return 0
+            }
+
+            if num < 0 {
+                negativeCount += 1
+            }
+        }
+
+        return negativeCount & 1 == 0 ? 1 : -1
+    }
+}
