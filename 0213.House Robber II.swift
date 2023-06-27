d9578fdbf228e2d87abc79b050ed5344d0ddf354
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-06-27 14:52:28 +0300

    Update 0213.House Robber II.swift

--- 0213.House Robber II.swift
+++ 0213.House Robber II.swift
@@ -17,13 +17,13 @@ class Solution {
         dp2[1] = nums[1]
         
         for i in 2..<n {
-            dp1[i] = nums[i] + dp1[i-2]
-            dp1[i-1] = max(dp1[i-1],dp1[i-2])
+            dp1[i] = nums[i] + dp1[i - 2]
+            dp1[i - 1] = max(dp1[i - 1], dp1[i - 2])
             
-            dp2[i] = nums[i] + dp2[i-2]
-            dp2[i-1] = max(dp2[i-1],dp2[i-2])
+            dp2[i] = nums[i] + dp2[i - 2]
+            dp2[i - 1] = max(dp2[i - 1], dp2[i - 2])
         }
         
-        return max(dp1[n-2], dp2[n-1])
+        return max(dp1[n - 2], dp2[n - 1])
     }
 }
