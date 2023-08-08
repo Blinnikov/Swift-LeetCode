0591679d16d16e68fb33e0aca77c4673570a679b
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-08-08 14:43:25 +0300

    Update 0033.Search in Rotated Sorted Array.swift

--- 0033.Search in Rotated Sorted Array.swift
+++ 0033.Search in Rotated Sorted Array.swift
@@ -2,8 +2,10 @@ class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
         let last = nums.count - 1
         var lo = 0, hi = last
+        
         while lo < hi {
             var mid = lo + (hi - lo) / 2
+            
             if (nums[mid] - nums[last]) * (target - nums[last]) > 0 {
                 if nums[mid] < target {
                     lo = mid + 1
