f76b14d592a531b3e9b0a43d1a95f6c78d9d9ba0
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-08-14 20:29:55 +0300

    Update 0215.Kth Largest Element in an Array.swift

--- 0215.Kth Largest Element in an Array.swift
+++ 0215.Kth Largest Element in an Array.swift
@@ -1,5 +1,34 @@
 class Solution {
     func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-        nums.sorted(by: >)[k-1]
+        // nums.sorted()[nums.count - k]
+        
+        var nums = nums
+
+        return quickSelect(&nums, 0, nums.count - 1, k)
+    }
+
+    private func quickSelect(_ nums: inout [Int], _ lo: Int, _ hi: Int, _ k: Int) -> Int {
+        var pivot = lo;
+
+        for i in lo..<hi {
+            if nums[i] <= nums[hi] {
+                nums.swapAt(i, pivot)
+                pivot += 1
+            }
+        }
+
+        nums.swapAt(hi, pivot)
+
+        let count = hi - pivot + 1
+
+        if count == k {
+            return nums[pivot]
+        }
+
+        if count > k {
+            return quickSelect(&nums, pivot + 1, hi, k)
+        }
+
+        return quickSelect(&nums, lo, pivot - 1, k - count)
     }
 }
