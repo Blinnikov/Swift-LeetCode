064f41feea2d1635ff5a0232e9eb9fe25f63788b
Igor Blinnikov <Blinnikov@users.noreply.github.com>
2023-10-12 08:45:23 +0300

    Update 0852.Peak Index in a Mountain Array.swift

--- 0852.Peak Index in a Mountain Array.swift
+++ 0852.Peak Index in a Mountain Array.swift
@@ -2,4 +2,21 @@ class Solution {
     func peakIndexInMountainArray(_ arr: [Int]) -> Int {
         arr.firstIndex(of: arr.max()!)!
     }
+
+    func peakIndexInMountainArray(_ arr: [Int]) -> Int {
+        var lo = 0
+        var hi = arr.count - 1
+
+        while lo < hi {
+            let mid = (lo + hi) / 2
+
+            if arr[mid] < arr[mid + 1] {
+                lo = mid + 1
+            } else {
+                hi = mid
+            }
+        }
+
+        return lo
+    }
 }
